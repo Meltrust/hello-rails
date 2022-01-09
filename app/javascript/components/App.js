18adebf5fe3e5b80e0e29d3c888eb453817c963f
meltrust <original.mtapia@outlook.com>
2022-01-08 22:53:28 -0600

    Add dependencies, add App.js

---
+++ app/javascript/components/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HelloWorld from './HelloWorld'
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" render={() => ("Home!")} />
+          <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
